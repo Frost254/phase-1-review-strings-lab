fc80d3dc261ff9a33b45fa9d821e9ff8576fac76
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-16 14:07:28 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+let currentUser = 'Amy';
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage += currentUser;
+//welcomeMessage = "Welcome to Flatbook, "
+welcomeMessage += '!';
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+excitedWelcomeMessage += currentUser.toUpperCase();
+excitedWelcomeMessage += '!';
+
+
+let shortGreeting = "Welcome, ";
+shortGreeting += currentUser[0];
+shortGreeting += '!';
